56d0460e791fd9e8e19577c8a9a4c6ecec4c6434
joshhigg <136753578+joshhigg@users.noreply.github.com>
2023-11-10 11:07:03 -0700

    added schema info

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+require('dotenv').config();
 
-const db = mysql.createConnection
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+db.connect((err) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log('connected to database')
+})
 
 inquirer.prompt([
     {
